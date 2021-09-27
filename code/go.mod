1ec7d02659789c8e3ab452c1b67f7ab9b02e9c66
Cristovao Cordeiro <cris@sixsq.com>
2021-09-27 11:32:18 +0200

    refactor example code

--- code/go.mod
+++ code/go.mod
@@ -3,6 +3,5 @@ module docker.ch/meetup
 go 1.16
 
 require (
-	github.com/google/gousb v1.1.1
 	github.com/sirupsen/logrus v1.8.1
 )
--- code/meetup.go
+++ code/meetup.go
@@ -1,60 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"runtime/debug"
-	"time"
+	"net/http"
 
-	"github.com/google/gousb"
-	"github.com/google/gousb/usbid"
 	log "github.com/sirupsen/logrus"
 )
 
-var lsusb_functional bool = false
-
-
-func onContextError() {
-	if !lsusb_functional {
-		log.Warn("Unable to initialize USB discovery. Host might be incompatible with this peripheral manager. Trying again later...")
-		time.Sleep(10 * time.Second)
-		log.Info(string(debug.Stack()))
-		os.Exit(0)
-	}
-}
-
-func get_usb_context() *gousb.Context {
-	defer onContextError()
-	c := gousb.NewContext()
-	lsusb_functional = true
-
-	return c
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/index.html")
 }
 
 func main() {
-	log.Info("meetup: USB finder has started")
+	log.Info("Docker CH Meetup: starting web app")
 
-	ctx := get_usb_context()
-	defer ctx.Close()
+	http.HandleFunc("/", rootHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-		devices, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
-			identifier := fmt.Sprintf("%s:%s", desc.Vendor, desc.Product)
-			vendor := usbid.Vendors[desc.Vendor]
-			product := vendor.Product[desc.Product]
-
-			description := fmt.Sprintf("USB device [%s] with ID %s. Protocol: %s",
-				product,
-				identifier,
-				usbid.Classify(desc))
-
-                        log.Info(description)
-
-			return false
-		})
-
-	if err != nil {
-		log.Error("Unable to discover USB devices")
-	}
-
-	log.Infof("Found %d USB devices", len(devices))
+	http.ListenAndServe(":8080", nil)
 }
